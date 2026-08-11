50404e70bb84c6e3d04aef42a6e6fbadf4288f1a
crumble-source <t.n.may9@gmail.com>
2026-08-12 08:48:47 +0900

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,1 +1,29 @@
+// クランブル徹底攻略
+// メインページ用JavaScript
 
+console.log("クランブル徹底攻略へようこそ！");
+
+
+// ページ内リンクをクリックしたときの処理
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(event) {
+
+        const target = document.querySelector(
+            this.getAttribute("href")
+        );
+
+        if (target) {
+
+            event.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        }
+
+    });
+
+});
